f06125e9f93efba3f2f06fbaeb30585fbadf64ed
David Youster <davidyouster@gmail.com>
2016-05-12 15:04:25 +0100

    Store images in separate directory.

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ const PORT = 8000
 function onRequest(socket) {
   console.log('Client connected...')
   const timestamp = new Date().getTime()
-  socket.pipe(fs.createWriteStream('img' + timestamp + '.jpg'))
+  socket.pipe(fs.createWriteStream('images/img' + timestamp + '.jpg'))
 }
 
+if (!fs.existsSync('images')) {
+  fs.mkdirSync('images');
+}
 net.createServer(onRequest).listen(PORT, HOST)
