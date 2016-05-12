cbd24c7e9cd7650214cb26b978ce526aee59862b
David Youster <davidyouster@gmail.com>
2016-05-12 17:16:58 +0100

    Display message on startup.

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const net = require('net')
 const fs = require('fs')
+const os = require('os')
 
 const HOST = '0.0.0.0'
 const PORT = 8000
+
+function getNetworkIP() {
+  return os.networkInterfaces().wlan0[0].address;
+}
 
 function onRequest(socket) {
   console.log('Client connected...')
@@ -13,4 +18,6 @@ function onRequest(socket) {
 if (!fs.existsSync('images')) {
   fs.mkdirSync('images');
 }
+
 net.createServer(onRequest).listen(PORT, HOST)
+console.log('Server listening on ' + getNetworkIP() + ':' + PORT)
