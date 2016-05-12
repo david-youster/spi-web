f31b4381fc33bba93ac7da21f1d5c038aa8211d7
David Youster <davidyouster@gmail.com>
2016-05-12 14:53:56 +0100

    Indent with two spaces.

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const HOST = '0.0.0.0'
 const PORT = 8000
 
 function onRequest(socket) {
-    console.log('Client connected...')
-    const timestamp = new Date().getTime()
-    socket.pipe(fs.createWriteStream('img' + timestamp + '.jpg'))
+  console.log('Client connected...')
+  const timestamp = new Date().getTime()
+  socket.pipe(fs.createWriteStream('img' + timestamp + '.jpg'))
 }
 
 net.createServer(onRequest).listen(PORT, HOST)
